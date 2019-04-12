ebe1a982c58ac7fcd2a55fe74b029968941047dd
padhupradheep <padhupradheep@gmail.com>
2019-04-12 15:48:33 +0200

    Sample pick and place is added

--- move_robot.py
+++ move_robot.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+
+# Created on: December, 2019
+# Author(s): Pradheep Krishna Muthukrishnan Padmanabhan
+# Copyright (c) [2017]
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import sys
 import copy
 import rospy
@@ -91,6 +112,7 @@ class Manipulation():
 		group.go()
 		self.gripper.set_named_target("close")
 		self.gripper.go()
+		rospy.sleep(8)
 		self.group.set_named_target("initial")
 		self.group.go()
 
@@ -126,6 +148,8 @@ class Manipulation():
 		group.go()
 		self.gripper.set_named_target("open")
 		self.gripper.go()
+		self.group.set_named_target("initial")
+		self.group.go()
 		moveit_commander.roscpp_shutdown()
 
 
@@ -177,3 +201,4 @@ def main():
 
 if __name__=='__main__':
 	main()
+
